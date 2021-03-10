099dd571cce1a6c4dabbc2f861c61ce1948f602d
Stephan Hagemann <stephan.hagemann@gusto.com>
2021-03-10 13:04:42 -0500

    Fix code extractor

--- chapter_code_extractor.sh
+++ chapter_code_extractor.sh
@@ -17,26 +17,14 @@ extractChapter () {
     fi
 
     rm -rf $OUT
-    mkdir $OUT
+    mkdir -p $OUT
     FILE=$(find docker/minio/data/releases/$SRC* | sort | tail -1)
     tar -xzf $FILE -C $OUT
 }
 
-extractChapter "c2s01"
-extractChapter "c2s02"
-extractChapter "c2s03"
-extractChapter "c3s01"
-extractChapter "c3s02"
-extractChapter "c3s03"
-extractChapter "c3s04"
-extractChapter "c3s07"
-extractChapter "c4s02"
-extractChapter "c4s04_1" "c4s04_book"
-extractChapter "c4s04_2" "c4s04_continued_1"
-extractChapter "c4s04_3" "c4s04_continued_2"
-extractChapter "c4s04_4" "c4s04_finished"
-extractChapter "c4s05_1" "c4s05_book"
-extractChapter "c4s05_2" "c4s05_finished"
-extractChapter "c4s06"
-extractChapter "c6s01"
-extractChapter "c6s02"
+extractChapter "c2s01" "../code_public/c2s01"
+extractChapter "c2s02" "../code_public/c2s02"
+extractChapter "c2s03" "../code_public/c2s03"
+extractChapter "c2s04" "../code_public/c2s04"
+extractChapter "c2s05" "../code_public/c2s05"
+
