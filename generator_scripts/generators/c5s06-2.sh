978047f59cdc33c4368f87a11e4604433d5aa94e
Stephan Hagemann <stephan.hagemann@gmail.com>
2021-07-12 07:09:40 -0600

    Add c5s06-2

---
+++ generator_scripts/generators/c5s06-2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -v
+set -x
+set -e
+
+echo "
+gem 'sorbet', :group => :development
+gem 'sorbet-runtime'
+gem 'image_processing', '~> 1.2'
+" > Gemfile
+
+bundle install --local
+
+bundle exec srb typecheck -e 'puts "Hello, world!"'
+
+bundle exec ruby -e 'puts(require "sorbet-runtime")'
+
+bundle exec srb init
+
+echo '# typed: strict
+
+module PredictionUi
+  extend T::Sig
+
+  sig {params(predictor: T.class_of(Predictor)).void}
+  def self.configure(predictor)
+    @predictor = T.let(predictor, T.nilable(T.class_of(Predictor)))
+    freeze
+  end
+
+  sig {returns(T.nilable(T.class_of(Predictor)))}
+  def self.predictor
+    @predictor
+  end
+end
+' > packages/prediction_ui/app/services/prediction_ui.rb
+
+bundle exec srb tc
+
+find . -iname 'deprecated_references.yml' -delete
+
+bin/packwerk update-deprecations
+bin/packwerk validate
+bin/rake pocky:generate[root]
