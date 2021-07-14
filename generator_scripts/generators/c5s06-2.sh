549252443ee600f10c10d35391028c16a77d74a3
Stephan Hagemann <stephan.hagemann@gmail.com>
2021-07-14 07:01:44 -0600

    Use known good sorbet config

--- generator_scripts/generators/c5s06-2.sh
+++ generator_scripts/generators/c5s06-2.sh
@@ -6,7 +6,32 @@ set -e
 
 bundle install --local
 
-SRB_YES=1 bundle exec srb init
+# This next step iswhat we should be doing.
+# However, this step behaves differently between different OSes and setups.
+# So instead of running this script we copy a known good configuration of sorbet into the app
+
+# SRB_YES=1 bundle exec srb init
+
+for FILE in `find ../../generator-scripts-repo/generator_scripts/generators/c5s06-2 -type f`
+do
+  array=()
+  DELIMITER='c2s01/'
+  PARTS=$FILE$DELIMITER
+  while test "$PARTS"
+  do
+      array+=( "${PARTS%%"$DELIMITER"*}" )
+      PARTS=${PARTS#*"$DELIMITER"}
+  done
+  NEW_FILENAME=${array[1]}
+  NEW_FILEPATH=$(/usr/bin/dirname $NEW_FILENAME)
+
+  echo ${FILE}
+  echo ${NEW_FILENAME}
+  echo ${NEW_FILEPATH}
+
+  mkdir -p -p ${NEW_FILEPATH}
+  cp $FILE ${NEW_FILEPATH}
+done
 
 echo '# typed: strict
 
