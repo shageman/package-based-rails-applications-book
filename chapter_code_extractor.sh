22d178f262956f796553a610cb131f3b5d532915
Stephan Hagemann <stephan.hagemann@gmail.com>
2021-09-20 07:32:10 -0600

    Extract all chapters for consistency

--- chapter_code_extractor.sh
+++ chapter_code_extractor.sh
@@ -22,21 +22,21 @@ extractChapter () {
     tar -xzf $FILE -C $OUT
 }
 
-# extractChapter "c2s01" "c2s01"
-# extractChapter "c2s02" "c2s02"
-# extractChapter "c2s03" "c2s03"
-# extractChapter "c2s04" "c2s04"
-# extractChapter "c2s05" "c2s05"
-# extractChapter "c2s06" "c2s06"
+extractChapter "c2s01" "c2s01"
+extractChapter "c2s02" "c2s02"
+extractChapter "c2s03" "c2s03"
+extractChapter "c2s04" "c2s04"
+extractChapter "c2s05" "c2s05"
+extractChapter "c2s06" "c2s06"
 
-# extractChapter "c4s01" "c4s01"
-# extractChapter "c4s02" "c4s02"
-# extractChapter "c4s03" "c4s03"
+extractChapter "c4s01" "c4s01"
+extractChapter "c4s02" "c4s02"
+extractChapter "c4s03" "c4s03"
 
-# extractChapter "c5s06-1" "c5s06-1"
-# extractChapter "c5s06-2" "c5s06-2"
-# extractChapter "c5s06-3" "c5s06-3"
-# extractChapter "c5s06-4" "c5s06-4"
+extractChapter "c5s06-1" "c5s06-1"
+extractChapter "c5s06-2" "c5s06-2"
+extractChapter "c5s06-3" "c5s06-3"
+extractChapter "c5s06-4" "c5s06-4"
 
 extractChapter "c5s07-1" "c5s07-1"
 extractChapter "c5s07-2" "c5s07-2"
