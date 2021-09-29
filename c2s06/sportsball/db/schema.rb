eec1de85ee443f8b3bfc827b07cf8e539d126c84
Stephan Hagemann <stephan.hagemann@gmail.com>
2021-09-28 20:49:48 -0600

    Update Chapter 2 to Rails 6.1.4.1

--- c2s06/sportsball/db/schema.rb
+++ c2s06/sportsball/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_022824) do
+ActiveRecord::Schema.define(version: 2021_09_24_175717) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "date"
