81973e15f7a6f9348b82dc3b9e360c66b9b924aa
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-01-02 07:46:23 -0700

    Import dependencies via importmap

---
+++ generator_scripts/generators/c2s01/config/importmap.rb
@@ -0,0 +1,10 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "foundation", to: "https://ga.jspm.io/npm:foundation@4.2.1-1/stylus/foundation.js"
+pin "path", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.14/nodelibs/browser/path.js"
+pin "stylus-type-utils", to: "https://ga.jspm.io/npm:stylus-type-utils@0.0.3/lib/type-utils.js"
