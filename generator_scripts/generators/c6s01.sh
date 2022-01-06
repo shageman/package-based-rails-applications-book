67467fb946209ec699af498c9cfaecec1ae7719d
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-01-06 05:04:22 -0700

    wip

--- generator_scripts/generators/c6s01.sh
+++ generator_scripts/generators/c6s01.sh
@@ -4,7 +4,7 @@ set -v
 set -x
 set -e
 
-# sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/games_admin/package.yml
-# sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/teams_admin/package.yml
-# sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/welcome_ui/package.yml
-# sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/prediction_ui/package.yml
+sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/games_admin/package.yml
+sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/teams_admin/package.yml
+sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/welcome_ui/package.yml
+sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/prediction_ui/package.yml
