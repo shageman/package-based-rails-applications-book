900a5e0d8e3cc88b44321df334763217f0e4c700
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-01-27 08:11:25 -0700

    Use object creation methods for all tests

---
+++ generator_scripts/generators/c2s01/spec/views/teams/new.html.slim_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe "teams/new", type: :view do
+  before(:each) do
+    assign(:team, new_team(
+      name: "MyString"
+    ))
+  end
+
+  it "renders new team form" do
+    render
+
+    assert_select "form[action=?][method=?]", teams_path, "post" do
+
+      assert_select "input[name=?]", "team[name]"
+    end
+  end
+end
