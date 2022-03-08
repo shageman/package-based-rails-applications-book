3c19c1b45d089f135af8e9f46307f3c1a06cc5fc
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-03-08 06:46:23 -0700

    Generate c4s03 out of c4s02

--- generator_scripts/generators/c4s03.sh
+++ generator_scripts/generators/c4s03.sh
@@ -11,37 +11,6 @@ set -e
 #
 ###############################################################################
 
-bundle install --local
-
-bundle gem testgem --no-coc --no-ext --no-mit --no-rubocop --test=rspec --ci=github --no-changelog
-
-rm -rf testgem/.git
-rm -rf testgem/.gitignore
-
-echo "# frozen_string_literal: true
-
-require_relative 'lib/testgem/version'
-
-Gem::Specification.new do |spec|
-  spec.name          = 'testgem'
-  spec.version       = Testgem::VERSION
-  spec.authors       = ['Stephan Hagemann']
-  spec.email         = ['stephan.hagemann@gmail.com']
-
-  spec.summary       = 'Write a short summary, because RubyGems requires one.'
-  spec.description   = 'Write a longer description or delete this line.'
-  spec.required_ruby_version = '>= 2.4.0'
-
-  spec.metadata['allowed_push_host'] = 'Set to http://mygemserver.com'
-
-  spec.files = Dir['app/**/*.rb', 'lib/**/*.rb'] + Dir['bin/*']
-  spec.bindir        = 'exe'
-  spec.executables   = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
-  spec.require_paths = ['lib']
-
-  spec.add_development_dependency 'zeitwerk'
-end" > testgem/testgem.gemspec
-
 echo "# frozen_string_literal: true
 
 if defined?(Rails)
