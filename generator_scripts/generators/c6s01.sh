1b5ece4586d1e747b7409ea5191e8f44dfa77c6d
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-03-10 18:40:22 -0700

    C6s01 Enforce privacy in root package

--- generator_scripts/generators/c6s01.sh
+++ generator_scripts/generators/c6s01.sh
@@ -41,6 +41,8 @@ dependencies:
 ' > packages/prediction_ui/package.yml
 
 
+sed -i 's/enforce_privacy: false/enforce_privacy: true/g' ./package.yml
+
 sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/games_admin/package.yml
 sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/teams_admin/package.yml
 sed -i 's/enforce_privacy: false/enforce_privacy: true/g' packages/welcome_ui/package.yml
