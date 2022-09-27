4a35d6e345c89ed307dc7a40e5ad0b8c2cebdbe0
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-09-27 06:56:37 -0600

    Add c7s01 to pipeline

---
+++ generator_scripts/generators/c7s01.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -v
+set -x
+set -e
+
+###############################################################################
+#
+# Add stimpack to codebase
+#
+###############################################################################
+
+echo "gem 'stimpack'" >> Gemfile
+
+bundle
+
+echo '--require spec_helper
+--require stimpack/rspec
+' > .rspec
+
+sed -i '/module Sportsball/i Stimpack.config.root = "packages"/g' config/application.rb
+
+sed -i '/append_view_path/d' packages/rails_shims/app/public/application_controller.rb 
+
