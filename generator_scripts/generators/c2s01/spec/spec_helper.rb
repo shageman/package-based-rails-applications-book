c658039380170cdf4f5bc58f3ac03dc881c80f1c
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-09-29 21:36:27 -0600

    Things that seem to need to change for Rails 7.0.4 on arm64 for the tests to pass

--- generator_scripts/generators/c2s01/spec/spec_helper.rb
+++ generator_scripts/generators/c2s01/spec/spec_helper.rb
@@ -37,3 +37,9 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+def wait_for_turbolinks timeout = nil
+  if has_css?('.turbolinks-progress-bar', visible: true, wait: (0.25).seconds)
+    has_no_css?('.turbolinks-progress-bar', wait: timeout.presence || 5.seconds)
+  end
+end
