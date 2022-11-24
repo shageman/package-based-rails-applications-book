5859fbf0612d4268888cf33c5d4dd6792204c1a7
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-11-24 14:21:34 -0700

    Add tester

---
+++ generator_scripts/ci/resource_tester.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -v
+set -x
+set -e
+
+# mkdir -p gems
+# export GEM_HOME=`pwd`/gems
+# echo $GEM_HOME
+# export SRB_PATH="/tmp/build/$(ls -1 /tmp/build)/gems/bin"
+# export PATH="$SRB_PATH:$PATH"
+
+# apt-get -y update
+# apt-get -y install ack graphviz
+
+# gem install bundler -v 2.3.4
+
+# tar -xzf code_input/$PREV_CHAPTER*.tgz -C code_output
+
+# cd code_output/sportsball
+
+# cp -r VENDORED_GEMS/* vendor/cache/ # get our saved local gems back
+
+# ../../generator-scripts-repo/generator_scripts/generators/$CHAPTER.sh
+
+# find . -iname 'deprecated_references.yml' -delete
+# bundle install --local
+# bundle exec packwerk update-deprecations
+# bin/rake pocky:generate[root]
+
+# cd ..
+# tar --exclude='tmp/*' --exclude='`pwd`/gems/*' -zcf $CHAPTER-`date +%Y%m%d%H%M%S`.tgz sportsball; echo "zipping done"
+
+
+ping -c 3 minio
+ping -c 3 concourse-db
+ping -c 3 concourse
