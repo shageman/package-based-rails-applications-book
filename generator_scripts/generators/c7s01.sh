cdb94d46c65cc39d223c855cc798c3c4bab52f53
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-11-24 06:41:48 -0700

    C7S01: Remove more things made obsolete by stimpack

--- generator_scripts/generators/c7s01.sh
+++ generator_scripts/generators/c7s01.sh
@@ -14,11 +14,14 @@ echo "gem 'stimpack'" >> Gemfile
 
 bundle
 
-echo '--require spec_helper
---require stimpack/rspec
-' > .rspec
-
 sed -i '/module Sportsball/i Stimpack.config.root = "packages"' config/application.rb
+sed -i '/config\.paths\.add/d' config/application.rb
 
-sed -i '/append_view_path/d' packages/rails_shims/app/public/application_controller.rb 
+echo "--require spec_helper
+--require stimpack/rspec
+" > .rspec
+
+sed -i '/append_view_path/d' packages/rails_shims/app/controllers/application_controller.rb 
+
+sed -i '/# Adjust RSpec configuration for package folder structure/,+50d' spec/spec_helper.rb
 
