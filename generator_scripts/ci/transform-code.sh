45e4770171a5bdebdc22ff275d7bea578a749c35
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-05-03 21:42:37 -0600

    Update transform code

--- generator_scripts/ci/transform-code.sh
+++ generator_scripts/ci/transform-code.sh
@@ -23,10 +23,10 @@ cp -r VENDORED_GEMS/* vendor/cache/ # get our saved local gems back
 
 ../../generator-scripts-repo/generator_scripts/generators/$CHAPTER.sh
 
-find . -iname 'deprecated_references.yml' -delete
+find . -iname 'package_todos.yml' -delete
 bundle install --local
-bundle exec packwerk update-deprecations
-bin/rake pocky:generate[root]
+bundle exec packwerk update
+bin/packs visualize
 
 cd ..
 tar --exclude='tmp/*' --exclude='`pwd`/gems/*' -zcf $CHAPTER-`date +%Y%m%d%H%M%S`.tgz sportsball; echo "zipping done"
