7794e962eb13814cf3d76d5902133d0a9fc5c0d8
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-06-22 23:48:23 -0600

    New chapter extraction code

--- chapter_code_extractor.sh
+++ chapter_code_extractor.sh
@@ -1,49 +1,58 @@
 #!/usr/bin/env bash
 
-extractChapter () {
-    SRC=$1
-    OUT=$2
+# INSTALL https://min.io/docs/minio/linux/reference/minio-mc.html?ref=docs
 
-    if [ -z $SRC ];
-        then echo "ERROR: call extractChapter as extractChapter(SRC[, OUT])"
+# brew install minio/stable/mc
+
+# bash +o history
+# mc alias set minio concourse-server.local:9001 minio minio123
+# bash -o history
+
+# mc alias set concourse-server http://concourse-server.local:9001 minio minio123
+
+
+extractChapter () {
+    CHAPTER=$1
+
+    if [ -z $CHAPTER ];
+        then echo "ERROR: call extractChapter as extractChapter(CHAPTER)"
         exit 1
     fi
 
-    echo "extractChapter for SRC $SRC"
+    echo "extractChapter for CHAPTER $CHAPTER"
 
-    if [ -z $OUT ]; then
-        echo "extractChapter called without OUT. Setting OUT to SRC ($SRC)"
-        OUT=$SRC
-    fi
-
-    rm -rf $OUT
-    mkdir -p $OUT
-    FILE=$(find docker/minio/data/releases/$SRC* | sort | tail -1)
-    tar -xzf $FILE -C $OUT
+    rm -rf $CHAPTER
+    mkdir -p $CHAPTER
+    FILE=$(mc ls concourse-server/releases/ | grep $CHAPTER | sort | tail -1 | awk '{print $NF}')
+    echo $FILE
+    echo $CHAPTER/$FILE
+    mc cp concourse-server/releases/$FILE $CHAPTER/$FILE
+    sleep 1
+    tar -xzf $CHAPTER/$FILE -C $CHAPTER
 }
 
-extractChapter "c2s01" "c2s01"
-extractChapter "c2s02" "c2s02"
-extractChapter "c2s03" "c2s03"
-extractChapter "c2s04" "c2s04"
-extractChapter "c2s05" "c2s05"
-extractChapter "c2s06" "c2s06"
+extractChapter "c2s01"
+extractChapter "c2s02"
+extractChapter "c2s03"
+extractChapter "c2s04"
+extractChapter "c2s05"
+extractChapter "c2s06"
 
-extractChapter "c4s01" "c4s01"
-extractChapter "c4s02" "c4s02"
-extractChapter "c4s03" "c4s03"
+extractChapter "c4s01"
+extractChapter "c4s02"
+extractChapter "c4s03"
 
-extractChapter "c5s07-1" "c5s07-1"
-extractChapter "c5s07-2" "c5s07-2"
-extractChapter "c5s07-3" "c5s07-3"
-extractChapter "c5s08" "c5s08"
-extractChapter "c5s09" "c5s09"
+extractChapter "c5s07-1"
+extractChapter "c5s07-2"
+extractChapter "c5s07-3"
+extractChapter "c5s08"
+extractChapter "c5s09"
 
-extractChapter "c6s01" "c6s01"
-extractChapter "c6s02" "c6s02"
-extractChapter "c6s03" "c6s03"
-extractChapter "c6s04-1" "c6s04-1"
-extractChapter "c6s04-2a" "c6s04-2a"
-extractChapter "c6s04-2b" "c6s04-2b"
+extractChapter "c6s01"
+extractChapter "c6s02"
+extractChapter "c6s03"
+extractChapter "c6s04-1"
+extractChapter "c6s04-2a"
+extractChapter "c6s04-2b"
 
-extractChapter "c7s01" "c7s01"
+extractChapter "c7s01"
