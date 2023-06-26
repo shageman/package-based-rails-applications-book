fc4d676a242b7f18745b77dd9bada91a2b1b8475
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-06-25 20:35:19 -0600

    Predictor updates

--- c6s02/sportsball/packages/predictor/app/public/predictor.rb
+++ c6s02/sportsball/packages/predictor/app/public/predictor.rb
@@ -12,7 +12,7 @@ class Predictor
     @teams_lookup = teams.inject({}) do |memo, team|
       memo[team.id] = TeamLookup.new(
         team: team,
-        rating: Saulabs::TrueSkill::Rating.new(1500.0, 1000.0, 1.0)
+        rating: ::Saulabs::TrueSkill::Rating.new(1500.0, 1000.0, 1.0)
       )
       memo
     end
@@ -23,7 +23,7 @@ class Predictor
       game_result = game.winning_team == 1 ?
           [[first_team_rating], [second_team_rating]] :
           [[second_team_rating], [first_team_rating]]
-        Saulabs::TrueSkill::FactorGraph.new(game_result, [1, 2]).update_skills
+        ::Saulabs::TrueSkill::FactorGraph.new(game_result, [1, 2]).update_skills
     end
   end
 
@@ -45,7 +45,7 @@ class Predictor
 
   class TeamLookup < T::Struct
     const :team, Contender
-    const :rating, Saulabs::TrueSkill::Rating
+    const :rating, ::Saulabs::TrueSkill::Rating
   end
   private_constant :TeamLookup
 end
--- c6s04-2a/sportsball/packages/predictor/app/public/predictor.rb
+++ c6s04-2a/sportsball/packages/predictor/app/public/predictor.rb
@@ -1,4 +1,4 @@
-# typed: strict
+# typed: false
 
 require "saulabs/trueskill"
 
@@ -12,25 +12,25 @@ class Predictor
     @teams_lookup = teams.inject({}) do |memo, team|
       memo[team.id] = TeamLookup.new(
         team: team,
-        rating: Saulabs::TrueSkill::Rating.new(1500.0, 1000.0, 1.0)
+        rating: ::Saulabs::TrueSkill::Rating.new(1500.0, 1000.0, 1.0)
       )
       memo
     end
 
     games.each do |game|
-      first_team_rating = @teams_lookup[game.first_team_id].rating
-      second_team_rating = @teams_lookup[game.second_team_id].rating
+      first_team_rating = @teams_lookup[game.first_team.id].rating
+      second_team_rating = @teams_lookup[game.second_team.id].rating
       game_result = game.winning_team == 1 ?
           [[first_team_rating], [second_team_rating]] :
           [[second_team_rating], [first_team_rating]]
-        Saulabs::TrueSkill::FactorGraph.new(game_result, [1, 2]).update_skills
+        ::Saulabs::TrueSkill::FactorGraph.new(game_result, [1, 2]).update_skills
     end
   end
 
   sig {override.params(first_team: Contender, second_team: Contender).returns(Prediction)}
   def predict(first_team, second_team)
-    team1 = T.must(T.must(@teams_lookup)[first_team.id]).team
-    team2 = T.must(T.must(@teams_lookup)[second_team.id]).team
+    team1 = T.must(T.must(@teams_lookup)[T.must(first_team.id)]).team
+    team2 = T.must(T.must(@teams_lookup)[T.must(second_team.id)]).team
     winner = higher_mean_team(first_team, second_team) ? team1 : team2
     Prediction.new(team1, team2, winner)
   end
@@ -39,8 +39,8 @@ class Predictor
 
   sig {params(first_team: Contender, second_team: Contender).returns(T::Boolean)}
   def higher_mean_team(first_team, second_team)
-    T.must(T.must(@teams_lookup)[first_team.id]).rating.mean >
-        T.must(T.must(@teams_lookup)[second_team.id]).rating.mean
+    T.must(T.must(@teams_lookup)[T.must(first_team.id)]).rating.mean >
+        T.must(T.must(@teams_lookup)[T.must(second_team.id)]).rating.mean
   end
 
   class TeamLookup < T::Struct
@@ -49,4 +49,5 @@ class Predictor
   end
   private_constant :TeamLookup
 end
+
 
