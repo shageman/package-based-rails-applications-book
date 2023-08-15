e50cae33339e367cba524a173795f3e15265492f
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-08-15 14:39:31 -0600

    Update rubocop files

--- c5s08/sportsball/config/rubocop_packs.rb
+++
@@ -1,9 +0,0 @@
-RuboCop::Packs.configure do |config|
-  config.permitted_pack_level_cops = %w(
-    Packs/ClassMethodsAsPublicApis
-    Packs/RootNamespaceIsPackName
-    Packs/TypedPublicApis
-    Packs/DocumentedPublicApis
-  )
-  config.required_pack_level_cops = %w()
-end
