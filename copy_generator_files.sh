8eb228e1fb97c3cff5e04a305589af919cc7eb7b
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-08-16 22:21:18 -0600

    Output when is happening when generator files get copied

--- copy_generator_files.sh
+++ copy_generator_files.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
 cp -R generator_scripts/ ../package-based-rails-applications-book/generator_scripts/
 
 find . -maxdepth 1 -type f | grep -v gitignore | xargs -I {} cp {} ../package-based-rails-applications-book
