48ecb3f682a85e86dedf622b32aa022deca5e0ec
Stephan Hagemann <stephan.hagemann@gmail.com>
2025-03-01 11:09:26 -0700

    Add new versions of C4

--- c4s05/sportsball/db/schema.rb
+++ c4s05/sportsball/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_16_173557) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_26_022701) do
   create_table "games", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
@@ -28,5 +28,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_16_173557) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
--- c4s07/sportsball/packs/teams_admin/app/controllers/teams_controller.rb
+++ c4s07/sportsball/packs/teams_admin/app/controllers/teams_controller.rb
@@ -25,7 +25,7 @@ class TeamsController < ApplicationController
 
     respond_to do |format|
       if @team.save
-        format.html { redirect_to team_url(@team), notice: "Team was successfully created." }
+        format.html { redirect_to @team, notice: "Team was successfully created." }
         format.json { render :show, status: :created, location: @team }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class TeamsController < ApplicationController
   def update
     respond_to do |format|
       if @team.update(team_params)
-        format.html { redirect_to team_url(@team), notice: "Team was successfully updated." }
+        format.html { redirect_to @team, notice: "Team was successfully updated." }
         format.json { render :show, status: :ok, location: @team }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class TeamsController < ApplicationController
     @team.destroy!
 
     respond_to do |format|
-      format.html { redirect_to teams_url, notice: "Team was successfully destroyed." }
+      format.html { redirect_to teams_path, status: :see_other, notice: "Team was successfully destroyed." }
       format.json { head :no_content }
     end
   end
