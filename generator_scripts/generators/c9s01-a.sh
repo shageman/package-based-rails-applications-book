ab3fa1d0b58a9abccd0273abe07028812c0fc3db
Stephan Hagemann <stephan.hagemann@gmail.com>
2025-03-01 11:54:27 -0700

    Simplify eventide implementation

--- generator_scripts/generators/c9s01-a.sh
+++ generator_scripts/generators/c9s01-a.sh
@@ -34,15 +34,18 @@ end" > sorbet/rbi/gems/prediction_component@0.1.0.rbi
 sed -i "1 i\require 'prediction_component/client' \
 " packs/games/app/models/game.rb
 sed -i '/belongs_to :second_team/a\ \
-  sig { void }\
-  def record_game_creation\
-    PredictionComponent::Client::RecordGameCreation.(\
+\
+  after_create :record_game_creation \
+\
+  sig { void } \
+  def record_game_creation \
+    PredictionComponent::Client::RecordGameCreation.( \
       league_id: 1, \
       game_id: self.id, \
       first_team_id: self.first_team_id, \
       second_team_id: self.second_team_id, \
-      winning_team: self.winning_team\
-    )\
+      winning_team: self.winning_team \
+    ) \
   end' packs/games/app/models/game.rb
 
 cat packs/games/app/models/game.rb
@@ -53,14 +56,14 @@ cat packs/games/app/models/game.rb
 sed -i "1 i\require 'prediction_component/client'" packs/prediction_ui/app/controllers/predictions_controller.rb
 sed -i '/def create/,+50d' packs/prediction_ui/app/controllers/predictions_controller.rb
 echo "  def create
-    team1id = params["first_team"]["id"]
-    team2id = params["second_team"]["id"]
+    team1id = params['first_team']['id']
+    team2id = params['second_team']['id']
 
     team1 = Team.find_by_id(team1id)
     team2 = Team.find_by_id(team2id)
 
-    ts1 = PredictionComponent::Client::FetchTeamStrength.(1, team1id)
-    ts2 = PredictionComponent::Client::FetchTeamStrength.(1, team2id)
+    ts1 = PredictionComponent::Client::FetchTeamStrength.(1, team1.id)
+    ts2 = PredictionComponent::Client::FetchTeamStrength.(1, team2.id)
 
     @prediction = Predictor::Prediction.new(team1, team2, ts1.mean > ts2.mean ? team1 : team2)
   end
